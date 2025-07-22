6eb7a5a4154e5e5d77f7487be2d83901b1389c0f
miya-algoma <miya@algomau.ca>
2025-07-22 17:46:32 +0000

    Main entry file updated

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
-<html>
-  <head>
-    <title>PHP Test</title>
-  </head>
-  <body>
-    <?php echo '<p>Hello World</p>'; ?> 
+<?php
+session_start();
+require_once 'config/database.php';
 
-</html>
+
+$url = $_SERVER['REQUEST_URI'];
+if (preg_match('#^/movies(?:/(\d+))?$#', $url, $matches)) {
+    require 'app/controllers/movies.php';
+    $controller = new Movies();
+    if (isset($matches[1])) {
+        $controller->show($matches[1]);
+    } else {
+        $controller->index();
+    }
+} else if ($url === '/movies/rate' && $_SERVER['REQUEST_METHOD'] === 'POST') {
+    require 'app/controllers/movies.php';
+    $controller = new Movies();
+    $controller->rate();
+} else {
+    echo "Page not found.";
+}
